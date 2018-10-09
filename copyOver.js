be9fa5bcba928730bcc405662f584cbfdfb2be52
Florian Mair <florian.mair@ml-pa.com>
2018-10-09 15:12:20 +0200

    remove postinstall log& add chalk

--- copyOver.js
+++ copyOver.js
@@ -1,9 +1,6 @@
 var ncp = require('ncp').ncp;
 var source = require('path').resolve()+'/templates',
-    destination = process.env.INIT_CWD +'/templates2';
-
-console.log(process.argv,require('path').resolve(),process.env.INIT_CWD)
-
+    destination = process.env.INIT_CWD +'/templates';
 ncp.limit = 16;
  
 ncp(source, destination, function (err) {
